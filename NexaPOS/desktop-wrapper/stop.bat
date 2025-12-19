cb374d0f645d0b9fa1bc0cce3a01580606fa5f2f
vickyvca <vickyv40@gmail.com>
2025-12-19 10:58:51 +0700

    Delete NexaPOS directory

--- NexaPOS/desktop-wrapper/stop.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Stop PHP built-in server by window title
-for /f "tokens=2" %%i in ('tasklist /FI "WINDOWTITLE eq php-server" /NH') do taskkill /PID %%i /F >nul 2>&1
-exit /b 0
--- NexaPOS/middleware.php
+++
@@ -1,5 +0,0 @@
-﻿<?php
-require_once __DIR__ . '/helpers.php';
-require_license();
-require_login();
-?>
